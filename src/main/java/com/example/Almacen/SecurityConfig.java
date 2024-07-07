30c2e9b979244c2a7d73bac30b98de615c481d1a
Sebas6422 <91403654+Sebas6422@users.noreply.github.com>
2024-07-07 01:26:46 -0500

    espero salga

--- src/main/java/com/example/Almacen/SecurityConfig.java
+++ src/main/java/com/example/Almacen/SecurityConfig.java
@@ -1,24 +1,48 @@
 package com.example.Almacen;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig implements WebMvcConfigurer{
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable() // Deshabilita CSRF por simplicidad en este ejemplo
-            .authorizeRequests()
-                .antMatchers("/public/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin().permitAll()
-                .and()
-            .logout().permitAll();
+                .headers(headers -> headers.contentSecurityPolicy("default-src 'self'"))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/css/**", "/js/**", "/imagenes/**").permitAll()
+                        .requestMatchers("/","Nosotros","/Login","/usuario/ingresarLogin","/AdminDashIn").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login -> login
+                        .loginPage("/Login")
+                        .loginProcessingUrl("/usuario/ingresarLogin")
+                        .defaultSuccessUrl("/AdminDashIn", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/usuario/cerrarSesion")
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                        .permitAll()
+                )
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                );
+
+        return http.build();
     }
+
+
 }
