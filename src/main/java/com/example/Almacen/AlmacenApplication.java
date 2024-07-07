5346d72bc67e5380902b8651455a49c03eb9ae38
Sebas6422 <91403654+Sebas6422@users.noreply.github.com>
2024-07-07 01:52:23 -0500

    correccion

--- src/main/java/com/example/Almacen/AlmacenApplication.java
+++ src/main/java/com/example/Almacen/AlmacenApplication.java
@@ -2,9 +2,10 @@ package com.example.Almacen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class AlmacenApplication {
+public class AlmacenApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlmacenApplication.class, args);
