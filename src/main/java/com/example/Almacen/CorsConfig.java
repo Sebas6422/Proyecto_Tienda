375ac65e1c749976a874288913529b7e6f55c597
Sebas6422 <91403654+Sebas6422@users.noreply.github.com>
2024-07-08 02:06:28 -0500

    cambios CSF con metatag

--- src/main/java/com/example/Almacen/CorsConfig.java
+++ src/main/java/com/example/Almacen/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://bodega-janys.azurewebsites.net/")
+                .allowedOrigins("http://localhost:9095", "https://bodega-janys.azurewebsites.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*") 
                 .allowCredentials(true); 
